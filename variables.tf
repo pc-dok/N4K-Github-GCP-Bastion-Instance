73296fa56ca08020b0b6018a0702ec1707f60fca
pc-dok <info@n4k.at>
2023-04-25 16:54:23 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,6 +15,18 @@ variable "GCP_Bastion_PW" {
   description = "Guacamole MySQL Password"
 }
 
+variable "GCP_Bastion_Admin_PW" {
+  type = string
+  sensitive = true
+  description = "Local Windows Admin Password"
+}
+
+variable "admin_name" {
+  type        = string
+  description = "Local Admin User"
+  default     = "localadm"
+}
+
 variable "project_name" {
   type        = string
   description = "Name of the Google Cloud project"
