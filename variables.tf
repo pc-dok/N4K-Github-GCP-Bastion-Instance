e6fb60ab298f3cf8e7ea81ba657b8b94bfce81a2
pc-dok <info@n4k.at>
2023-04-26 09:04:58 +0200

    variables

--- variables.tf
+++ variables.tf
@@ -8,6 +8,8 @@ variable "GCP_Bastion_CERT_Country" {}
 variable "GCP_Bastion_CERT_Location" {}
 variable "GCP_Bastion_CERT_ORG" {}
 variable "GCP_Bastion_Email" {}
+variable "GCP_Bastion_Admin_PW" {}
+variable "GCP_PROJECT" {}
 
 variable "gcp_credentials" {
   type = string
@@ -19,16 +21,6 @@ variable "GCP_Bastion_PW" {
   type = string
   sensitive = true
   description = "Guacamole MySQL Password"
-}
-
-variable "GCP_Bastion_Admin_PW" {
-  type = string
-  description = "Local Windows Admin Password"
-}
-
-variable "GCP_PROJECT" {
-  type = string
-  description = "Name of the Google Cloud project"
 }
 
 variable "admin_name" {
